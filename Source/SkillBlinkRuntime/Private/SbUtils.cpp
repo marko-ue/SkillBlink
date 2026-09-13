9c88b8d56f37eee7ccf0c0089cd2cc36e2123988
Marko Petric <markop20042004@gmail.com>
2026-09-13 21:00:11 +0200

    Add SbUtils Blueprint Function Library that returns the player state component

---
+++ Source/SkillBlinkRuntime/Private/SbUtils.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) Marko Petric & Yevhenii Selivanov
+
+#include "SbUtils.h"
+
+// Sb
+#include "Components/SbPlayerStateComponent.h"
+
+// Bomber
+#include "GameFramework/BmrPlayerState.h"
+#include "UtilityLibraries/BmrBlueprintFunctionLibrary.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(SbUtils)
+
+// Returns Player State Component with a function to broadcast the ability's gameplay event
+USbPlayerStateComponent* USbUtils::GetPlayerStateComponent(const UObject* OptionalWorldContext)
+{
+	const ABmrPlayerState* PlayerState = UBmrBlueprintFunctionLibrary::GetLocalPlayerState(OptionalWorldContext);
+	return PlayerState ? PlayerState->FindComponentByClass<USbPlayerStateComponent>() : nullptr;
+}
---
+++ Source/SkillBlinkRuntime/Public/SbUtils.h
@@ -0,0 +1,24 @@
+// Copyright (c) Marko Petric & Yevhenii Selivanov
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "SbUtils.generated.h"
+
+/**
+ * Static helper functions about SkillBlink.
+ */
+UCLASS()
+class SKILLBLINKRUNTIME_API USbUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	/*********************************************************************************************
+	 * Object getters
+	 ********************************************************************************************* */
+public:
+	/** Returns the Player State component of SkillBlink. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "[SkillBlink]", DisplayName = "Get Sb Player State Component", meta = (WorldContext = "OptionalWorldContext", CallableWithoutWorldContext))
+	static class USbPlayerStateComponent* GetPlayerStateComponent(const UObject* OptionalWorldContext = nullptr);
+};
